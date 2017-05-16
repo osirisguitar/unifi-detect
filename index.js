2999cc9c65523d6aba6c2ff14730ef94212dcac7
Anders Norrback Bornholm <anders@bornholm.se>
2017-05-16 20:33:42 +0200

    First working version

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const unifi = require('./lib/unifi');
+const domoticz = require('./lib/domoticz');
+const config = require('./lib/config');
+
+domoticz.init(config.domoticz.url);
+unifi.init(config.unifi);
+
+unifi.getDevices(config.unifi.url, config.unifi.host, config.unifi.user, config.unifi.password)
+  .then(devices => {
+    config.domoticz.deviceMappings.forEach(deviceMapping => {
+      let device = devices.find(device => {
+        return device.mac === deviceMapping.mac;
+      });
+
+      if (device) {
+        domoticz.turnOnSwitch(deviceMapping.switchId);
+      } else {
+        domoticz.turnOnSwitch(deviceMapping.switchId);
+      }
+    });
+  });
